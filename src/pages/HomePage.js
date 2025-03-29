5d894869fea7002e074210a882ddbf38c52e1a12
Sandeep <sandipsharma2445@gmail.com>
2025-03-29 17:41:57 +0530

    auto logout functinality added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,22 @@
-import StartingPageContent from '../components/StartingPage/StartingPageContent';
+import { useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import StartingPageContent from "../components/StartingPage/StartingPageContent";
+import LoginContext from "../store/LoginContext";
 
 const HomePage = () => {
+  const history = useHistory();
+  const { removeToken } = useContext(LoginContext);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      localStorage.removeItem("token");
+      removeToken();
+      history.replace("/auth"); 
+    }, 60 * 1000);
+
+    return () => clearTimeout(timer); 
+  }, [history]);
+
   return <StartingPageContent />;
 };
 
