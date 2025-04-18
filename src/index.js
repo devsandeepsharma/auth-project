4168fafef1dec5f3c1cb48149166e322db1cea70
Sandeep <sandipsharma2445@gmail.com>
2025-04-18 19:15:39 +0530

    bootstrap configured

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-import "./index.css";
+// React Bootstrap Configuration
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-bootstrap";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
